46a1e0fe3e72a605bbd91ee180fdfac5bfcb9d24
RaveeshaRukshani <raveesharukshani@gmaiil.com>
2020-03-14 17:21:09 +0530

    new album file

--- api/models/album.js
+++ api/models/album.js
@@ -1,11 +1,21 @@
 const mongoose=require('mongoose'); 
 
+
+
 const albumSchema = mongoose.Schema({
+
     _id: mongoose.Schema.Types.ObjectId,
+
     albumName: {type: String, required: true},
-    userList: {type: Array, required: true},
-    fileList: {type: Array, required: true}
+
+    userList: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+
+    fileList: [{type: mongoose.Schema.Types.ObjectId, ref: 'File'}]
+
+
 
 });
 
-module.exports = mongoose.model('User', userSchema);
+
+
+module.exports = mongoose.model('Album', albumSchema);
