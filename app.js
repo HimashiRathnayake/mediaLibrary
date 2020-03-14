bcb6226f023962f8303a6e41ea27aa35cd078609
RaveeshaRukshani <raveesharukshani@gmaiil.com>
2020-03-14 12:32:06 +0530

    handling error corrections

--- app.js
+++ app.js
@@ -14,19 +14,20 @@ app.use('/searchImages', searchImageRoutes);
 app.use('/searchAudios', searchAudioRoutes); 
 app.use('/searchVideos', searchVideoRoutes); 
 
-module.exports = app;
 
 app.use((req,res,next) => {
     const error = new Error('Not found');
-    error.status = 404;
+    error.status= 404 ;
     next(error);
 })
 
 app.use((error, req, res, next) => {
-    res.status(error.ststus || 500)
+    res.status(error.status || 500);
     res.json({
         error: {
             message: error.message
         }
     });
 });
+
+module.exports = app;
