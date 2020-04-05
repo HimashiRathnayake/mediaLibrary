8672f3ac2ed11f28811d6c62b5bb5ed8da5fb877
RaveeshaRukshani <raveesharukshani@gmaiil.com>
2020-04-05 13:36:34 +0530

    add signup validation

--- mediaLibrary-frontend/my-app/src/components/signupComponent.js
+++ mediaLibrary-frontend/my-app/src/components/signupComponent.js
@@ -2,7 +2,17 @@ import React, {Component} from 'react';
 import { Redirect, Link } from "react-router-dom";
 import './componentCss/login.css';
 import {SignUpData} from '../services/PostData';
-//import axios from 'axios';
+
+const emailRegex = RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/);
+
+const formValid = formErrors => {
+    let valid = true;
+
+    Object.values(formErrors).forEach(val => {
+        val.length>0 && (valid =false)
+    });
+    return valid;
+};
 
 export default class Signup extends Component{
     
@@ -16,45 +26,80 @@ export default class Signup extends Component{
             email: '',
             password: '',
             confirmPassword: '',
+            formErrors: {
+                email: '',
+                password: '',
+                confirmPassword: ''
+            },
             redirect: false
         }
-
     }  
 
     signup(){
-        console.log('signup clicked');
-        console.log(this.state);
-
-        SignUpData(this.state).then((result) => {
-            let responseJSON = result;
-            console.log("response:",responseJSON);
+        if(formValid(this.state.formErrors) && this.state.email.length != 0 && this.state.password.length != 0){
+            console.log(`
+                --submitting--
+                email: ${this.state.email}
+                password: ${this.state.password}
+            `)
+            SignUpData(this.state).then((result) => {
+                let responseJSON = result;
+                console.log("response:",responseJSON);
         
-            if(responseJSON.message==="User created"){
-                sessionStorage.setItem('userData', responseJSON);
-                this.setState({redirect: true})
-            }else{
-                console.log('SignUp Error');
-            }
-        })
+                if(responseJSON.token){
+                    sessionStorage.setItem('userData', JSON.stringify(responseJSON));
+                    this.setState({redirect: true})
+                }else{
+                    console.log('SignUp Error');
+                }
+            })
+        }
+        else{
+            console.error('form invalid - display error message');
+        }  
     }
 
     onChange(e){
+        const {name, value} = e.target;
+        let formErrors = this.state.formErrors;
+        let pass = '';
+
+        switch(name){
+            case 'email':
+                formErrors.email =
+                    emailRegex.test(value) && value.length > 0 
+                    ? ""
+                    :"invalid email address";
+                break;
+            case 'password':
+                pass= value;
+                formErrors.password =
+                    value.length < 3 && value.length >0 
+                    ? "minimum 3 characters required"
+                    : "";
+                break;
+            case 'confirmPassword':
+                formErrors.confirmPassword =
+                value !== this.state.password
+                    ? "not matching"
+                    : "";
+                break;
+            default:
+                break;             
+        } 
         this.setState({
+            formErrors,
             [e.target.name]: e.target.value 
         });
-        
+        console.log(this.state);
     }
 
     render(){
-
         if(this.state.redirect){
-            return(<Redirect to={'/login'} />);
+            return(<Redirect to={'/start'} />);
         }
-        
-        /* if(sessionStorage.getItem('userData')){
-            return(<Redirect to={'/start'}/>);
 
-        }  */
+        const {formErrors} = this.state;
 
         return(
             <div className="auth-wrapper">
@@ -70,6 +115,9 @@ export default class Signup extends Component{
                                 name= "email"
                                 onChange={this.onChange}
                             />
+                            {formErrors.email.length > 0 && (
+                                <span className="errorMessage">{formErrors.email}</span>
+                            )}
                         </div>
 
                         <div className="form-group">
@@ -80,6 +128,9 @@ export default class Signup extends Component{
                                 name= "password"
                                 onChange={this.onChange}
                             />
+                            {formErrors.password.length > 0 && (
+                                <span className="errorMessage">{formErrors.password}</span>
+                            )} 
                         </div>
 
                         <div className="form-group">
@@ -90,6 +141,9 @@ export default class Signup extends Component{
                                 name= "confirmPassword"
                                 onChange={this.onChange}
                             />
+                            {formErrors.confirmPassword.length > 0 && (
+                                <span className="errorMessage">{formErrors.confirmPassword}</span>
+                            )} 
                         </div>
 
                         
@@ -102,5 +156,4 @@ export default class Signup extends Component{
             </div>
         );
     }
-
 }
