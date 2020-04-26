baaa4f5b0fec15837624b9ba20087597c735ba2e
RaveeshaRukshani <47144186+RaveeshaRukshani@users.noreply.github.com>
2020-04-26 18:38:53 +0530

    Modify  favourite route

--- mediaLibrary-backend/api/routes/favorites.js
+++ mediaLibrary-backend/api/routes/favorites.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const favoritesController = require('../controllers/favorites');
 
-router.post('/add', checkAuth, favoritesController.addFavorites);
-router.delete('/remove', checkAuth, favoritesController.removeFavorites);
+router.post('/add/:type/:Id', checkAuth, favoritesController.addFavorites);
+router.delete('/remove/:Id', checkAuth, favoritesController.removeFavorites);
+router.get('/', checkAuth, favoritesController.favourites_get_all);
 
 module.exports = router;
