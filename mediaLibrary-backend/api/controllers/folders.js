7c0acffd3bd5972f32a980bfee34369e82f87c3c
RaveeshaRukshani <47144186+RaveeshaRukshani@users.noreply.github.com>
2020-04-26 11:50:54 +0530

    change some backend functionalities

--- mediaLibrary-backend/api/controllers/folders.js
+++ mediaLibrary-backend/api/controllers/folders.js
@@ -2,95 +2,144 @@ const mongoose = require('mongoose');
 
 const Folder =  require('../models/folder'); 
 let File;
+const folderType= ['Image', 'Audio', 'Video'];
 
 exports.folders_get_folders = (req, res, next) =>{
-    Folder.find({userList: req.userData.userId, folderType: req.params.type})
-    .exec()
-    .then(docs=>{
-        const response={
-            count: docs.length,
-            folders: docs.map(doc=>{
-                return{
-                    _id: doc._id,
-                    folderName: doc.folderName,
-                    userList: doc.userList,
-                    folderType: doc.folderType
-                }
-            })
-        };
-        res.status(200).json(response);
-    })
-    .catch(err=>{
-        res.status(500).json({
-            error:err
+    if(folderType.includes(req.params.type)){
+        Folder.find({userList: req.userData.userId, folderType: req.params.type})
+        .exec()
+        .then(docs=>{
+            const response={
+                count: docs.length,
+                folders: docs.map(doc=>{
+                    return{
+                        _id: doc._id,
+                        folderName: doc.folderName,
+                        userList: doc.userList,
+                        folderType: doc.folderType
+                    }
+                })
+            };
+            res.status(200).json(response);
+        })
+        .catch(err=>{
+            res.status(500).json({
+                error:err
+            });
         });
-    });
+    }
+    else{
+        res.status(409).json({
+            message: 'folderType is invalid'
+        });
+    }
+    
 }
 
 exports.folders_create_folder = (req, res, next) =>{
-    const folder =new Folder({
-        _id: new mongoose.Types.ObjectId(),
-        folderName: req.body.folderName,
-        userList: [req.userData.userId],
-        folderType: req.params.type
-    });
-    folder.save().then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: 'Folder created successfully'
+    if(folderType.includes(req.params.type)){
+        const folder =new Folder({
+            _id: new mongoose.Types.ObjectId(),
+            folderName: req.body.folderName,
+            userList: [req.userData.userId],
+            folderType: req.params.type
         });
-    }).catch(err=>{
-        console.log(err);
-        res.status(500).json({
-            error: err
+        folder
+        .save()
+        .then(result => {
+            //console.log(result);
+            res.status(201).json({
+                message: 'Folder created successfully',
+                folder: result
+            });
+        })
+        .catch(err=>{
+            //console.log(err);
+            res.status(500).json({
+                error: err
+            });
         });
-    });
+    }
+    else{
+        res.status(409).json({
+            message: 'FolderType is invalid'
+        });
+    }
+    
+    
 }
 
 exports.folders_rename_folder = (req, res, next) =>{
-    Folder.update({_id: req.params.folderId},{folderName: req.body.folderName})
-    .exec()
+    Folder.findOne({_id: req.params.folderId})
     .then(result => {
-        console.log(result);
-        res.status(200).json({
-            message: 'Folder renamed successfully'
-        });
+        if (result === null){
+            res.status(404).json({
+                message: 'Folder not found'
+            })
+        }
+        else if(req.body.folderName === undefined){
+            res.status(409).json({
+                message: 'folderName name is required'
+            });
+        }
+        else{
+            Folder.update({_id: req.params.folderId},{folderName: req.body.folderName})
+            .exec()
+            .then(result => {
+                //console.log(result);
+                res.status(200).json({
+                    message: 'Folder renamed successfully'
+                });
+            })
+        }
     })
     .catch(err=>{
-        console.log(err);
+        //console.log(err);
         res.status(500).json({
             error:err
         });
     });
 }
 
+
+
 exports.folders_delete_folder = (req, res, next) =>{
-    Folder.find({_id: req.params.folderId})
-    .exec()
-    .then(result=>{
-        if (result[0].folderType==='Image'){
-            File=require('../models/image');
-        }
-        else if (result[0].folderType==='Audio'){
-            File=require('../models/audio');
-        }else{
-            File=require('../models/video');
-        }
-        console.log(File);
-        File.deleteMany({folder: req.params.folderId}).exec().then((result)=>{
-            console.log(result)
-        })
-    })
-    .then(
-        Folder.deleteOne({_id: req.params.folderId}).exec()
-    )
+    Folder.findOne({_id: req.params.folderId})
     .then(result => {
-        res.status(200).json({
-            message: "Folder deleted"
-        });
-    })
+        if (result===null){
+            res.status(404).json({
+                message: 'Folder not found'
+            })
+        }
+        else{
+            Folder.find({_id: req.params.folderId})
+            .exec()
+            .then(result=>{
+                if (result[0].folderType==='Image'){
+                    File=require('../models/image');
+                }
+                else if (result[0].folderType==='Audio'){
+                    File=require('../models/audio');
+                }else{
+                    File=require('../models/video');
+                }
+                //console.log(File);
+                File.deleteMany({folder: req.params.folderId}).exec().then((result)=>{
+                    //console.log(result)
+                })
+            })
+            .then(
+                Folder.deleteOne({_id: req.params.folderId}).exec()
+            )
+            .then(result => {
+                res.status(200).json({
+                    message: "Folder deleted"
+                });
+            })  
+        }
+    }) 
     .catch(err=>{
-        console.log(err);
+        //console.log(err);
         res.status(500).json({
             error: err
         });
