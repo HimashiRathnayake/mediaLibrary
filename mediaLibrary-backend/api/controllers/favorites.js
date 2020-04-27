ccd5a22b39c3ca6c1939ed291cd4531cc0fb032d
RaveeshaRukshani <47144186+RaveeshaRukshani@users.noreply.github.com>
2020-04-27 18:08:34 +0530

    modify favourite controller

--- mediaLibrary-backend/api/controllers/favorites.js
+++ mediaLibrary-backend/api/controllers/favorites.js
@@ -1,96 +1,336 @@
 const mongoose = require('mongoose');
 const User =  require('../models/user');
+const Image =  require('../models/image');
+const Audio =  require('../models/audio');
+const Video =  require('../models/video');
 
 exports.addFavorites =  (req, res, next) => {
     const id= req.userData.userId;
     const type= req.params.type;
     const fileId= req.params.Id;
+    
+    if (type=== 'Image'){
+        Image.findOne({_id: fileId})
+        .then(result=>{
+            if (result===null){
+                res.status(404).json({
+                    message: 'Image not found'
+                })
+            }
+            else{
+                User.findOne({_id: id})
+                .exec()
+                .then(function(user){
+                    const isInFavourite= user.imgfavourites.some(function(favourite){
+                        return (favourite._id.equals(mongoose.Types.ObjectId(fileId)));
+                    });
 
-    User.findById(id)
-    .then(function(user){
-        console.log("user:", user);
-        if(!user)
-        {
-            return res.sendStatus(401);
-        } 
-                
-        const isInFavourite= user.favourites.some(function(favourite){
-            return (favourite._id.equals(mongoose.Types.ObjectId(fileId)));
-        });
-        
-        if(isInFavourite){
-            res.status(400).json({
-                message: 'already have  favorites',
-                favourites: user.favourites
-            });
-        }
-        else{
-            const file = {_id: fileId, type: type, addedDate: new Date().toLocaleDateString()};
-            user.favourites.push(file);
-            user
-            .save()
-            .then(result => {
-                console.log(result);
-                res.status(201).json({
-                    message: 'add favorites',
-                    favourites: result.favourites
+                    if(isInFavourite){
+                        res.status(201).json({
+                            message: 'Already in favorites',
+                            imgfavourites: user.imgfavourites
+                        });
+                    }
+                    else{
+                        user.imgfavourites.push(fileId);
+                        user
+                        .save()
+                        .then(result => {
+                            console.log(result);
+                            res.status(200).json({
+                                message: 'Add image to favourites',
+                                imgfavourites: result.imgfavourites
+                            })
+                        });
+                    }
                 });
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
             });
-        }   
-    }).catch(next);  
+        }); 
+    }
+    else if (type=== 'Audio'){
+        Audio.findOne({_id: fileId})
+        .then(result=>{
+            console.log('results:', result);
+            if (result===null){
+                res.status(404).json({
+                    message: 'Audio not found'
+                })
+            }
+            else{
+                User.findOne({_id: id})
+                .exec()
+                .then(function(user){
+                    const isInFavourite= user.audfavourites.some(function(favourite){
+                        return (favourite._id.equals(mongoose.Types.ObjectId(fileId)));
+                    });
+
+                    if(isInFavourite){
+                        res.status(201).json({
+                            message: 'Already in favorites',
+                            audfavourites: user.audfavourites
+                        });
+                    }
+                    else{
+                        user.audfavourites.push(fileId)
+                        user
+                        .save()
+                        .then(result => {
+                            res.status(200).json({
+                                message: 'Add audio to favourites',
+                                audfavourites: result.audfavourites
+                            })
+                        }); 
+                    }
+                });
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        }); 
+    }else{
+        Video.findOne({_id: fileId})
+        .then(result=>{
+            console.log('results:', result);
+            if (result===null){
+                res.status(404).json({
+                    message: 'Video not found'
+                })
+            }
+            else{
+                User.findOne({_id: id})
+                .exec()
+                .then(function(user){
+                    const isInFavourite= user.vidfavourites.some(function(favourite){
+                        return (favourite._id.equals(mongoose.Types.ObjectId(fileId)));
+                    });
+
+                    if(isInFavourite){
+                        res.status(201).json({
+                            message: 'Already in favorites',
+                            vidfavourites: user.vidfavourites
+                        });
+                    }
+                    else{
+                        user.vidfavourites.push(fileId)
+                        user
+                        .save()
+                        .then(result => {
+                            res.status(200).json({
+                                message: 'Add video to favourites',
+                                vidfavourites: result.vidfavourites
+                            })
+                        }); 
+                    }
+                });
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        }); 
+    }
 }
 
 exports.removeFavorites =  (req, res, next) => {
     const id= req.userData.userId;
+    const type= req.params.type;
     const fileId= req.params.Id;
-    
-    User.findById(id)
-    .then(function(user){
-        console.log("user:", user);
-        if(!user)
-        {
-            return res.sendStatus(401);
-        } 
-        
-        const isInFavourite= user.favourites.some(function(favourite){
-            return (favourite._id.equals(mongoose.Types.ObjectId(fileId)));
-        });
-        
-        if(isInFavourite){
-            user.favourites.remove(fileId);
-            user
-            .save()
-            .then(result => {
-                console.log(result);
-                res.status(201).json({
-                    message: 'remove favorites',
-                    favourites: result.favourites
-                });
-            });
-        }
-        else{
-            res.status(201).json({
-                message: 'not in favorites',
-                favourites: user.favourites
-            });
-        }  
-    }).catch(next);  
 
+    if (type=== 'Image'){
+        Image.findOne({_id: fileId})
+        .then(result=>{
+            if (result===null){
+                res.status(404).json({
+                    message: 'Image not found'
+                })
+            }
+            else{
+                User.findOne({_id: id})
+                .exec()
+                .then(function(user){
+                    const isInFavourite= user.imgfavourites.some(function(favourite){
+                        return (favourite._id.equals(mongoose.Types.ObjectId(fileId)));
+                    });
+
+                    if(isInFavourite){
+                        user.imgfavourites.remove(fileId);
+                        user
+                        .save()
+                        .then(result => {
+                            console.log(result);
+                            res.status(200).json({
+                                message: 'Remove image from favorites',
+                                imgfavourites: result.imgfavourites
+                            });
+                        });
+                    }
+                    else{
+                        res.status(201).json({
+                            message: 'Image not in favorites',
+                            imgfavourites: user.imgfavourites
+                        });
+                    }
+                });
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        }); 
+    }
+    else if (type=== 'Audio'){
+        Audio.findOne({_id: fileId})
+        .then(result=>{
+            if (result===null){
+                res.status(404).json({
+                    message: 'Audio not found'
+                })
+            }
+            else{
+                User.findOne({_id: id})
+                .exec()
+                .then(function(user){
+                    const isInFavourite= user.audfavourites.some(function(favourite){
+                        return (favourite._id.equals(mongoose.Types.ObjectId(fileId)));
+                    });
+
+                    if(isInFavourite){
+                        user.audfavourites.remove(fileId);
+                        user
+                        .save()
+                        .then(result => {
+                            console.log(result);
+                            res.status(200).json({
+                                message: 'Remove audio from favorites',
+                                audfavourites: result.audfavourites
+                            });
+                        });
+                    }
+                    else{
+                        res.status(201).json({
+                            message: 'Audio not in favorites',
+                            audfavourites: user.audfavourites
+                        });
+                    }
+                });
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        }); 
+    }
+    else{
+        Video.findOne({_id: fileId})
+        .then(result=>{
+            if (result===null){
+                res.status(404).json({
+                    message: 'Video not found'
+                })
+            }
+            else{
+                User.findOne({_id: id})
+                .exec()
+                .then(function(user){
+                    const isInFavourite= user.vidfavourites.some(function(favourite){
+                        return (favourite._id.equals(mongoose.Types.ObjectId(fileId)));
+                    });
+
+                    if(isInFavourite){
+                        user.vidfavourites.remove(fileId);
+                        user
+                        .save()
+                        .then(result => {
+                            console.log(result);
+                            res.status(200).json({
+                                message: 'Remove video from favorites',
+                                vidfavourites: result.vidfavourites
+                            });
+                        });
+                    }
+                    else{
+                        res.status(201).json({
+                            message: 'Video not in favorites',
+                            vidfavourites: user.vidfavourites
+                        });
+                    }
+                });
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        }); 
+    }
+    
 }
 
 exports.favourites_get_all = (req, res, next) =>{
     const id= req.userData.userId;
-    User.findById(id)
-    .exec()
-    .then(user => {
-        res.status(200).json({
-            favourites: user.favourites
+    const type= req.params.type;
+
+    if(type ==='Image'){
+        User.findById(id)
+        .populate('imgfavourites')
+        .exec()
+        .then(user => {
+            res.status(200).json({
+                imgfavourites: user.imgfavourites
+            });
+        })
+        .catch(err=>{
+            res.status(500).json({
+                error:err
+            });
         });
-    })
-    .catch(err=>{
-        res.status(500).json({
-            error:err
+    }
+    else if(type ==='Audio'){
+        User.findById(id)
+        .populate('audfavourites')
+        .exec()
+        .then(user => {
+            res.status(200).json({
+                audfavourites: user.audfavourites
+            });
+        })
+        .catch(err=>{
+            res.status(500).json({
+                error:err
+            });
         });
-    });
+    }
+    else{
+        User.findById(id)
+        .populate('vidfavourites')
+        .exec()
+        .then(user => {
+            res.status(200).json({
+                vidfavourites: user.vidfavourites
+            });
+        })
+        .catch(err=>{
+            res.status(500).json({
+                error:err
+            });
+        });
+    }
+    
 }
 
