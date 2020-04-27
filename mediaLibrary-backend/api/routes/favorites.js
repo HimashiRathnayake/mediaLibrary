3d1b80ccc39147b53d474705eefffdf30d45aa7c
RaveeshaRukshani <47144186+RaveeshaRukshani@users.noreply.github.com>
2020-04-27 18:09:54 +0530

    modify favourite route

--- mediaLibrary-backend/api/routes/favorites.js
+++ mediaLibrary-backend/api/routes/favorites.js
@@ -4,7 +4,7 @@ const checkAuth = require('../middleware/check-auth');
 const favoritesController = require('../controllers/favorites');
 
 router.post('/add/:type/:Id', checkAuth, favoritesController.addFavorites);
-router.delete('/remove/:Id', checkAuth, favoritesController.removeFavorites);
-router.get('/', checkAuth, favoritesController.favourites_get_all);
+router.delete('/remove/:type/:Id', checkAuth, favoritesController.removeFavorites);
+router.get('/:type', checkAuth, favoritesController.favourites_get_all);
 
 module.exports = router;
