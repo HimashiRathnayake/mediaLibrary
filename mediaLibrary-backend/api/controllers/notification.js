1b71d9d9d5483fce053021707941d374256df52c
RaveeshaRukshani <47144186+RaveeshaRukshani@users.noreply.github.com>
2020-08-30 08:28:17 +0530

    add read and delete functionalities

--- mediaLibrary-backend/api/controllers/notification.js
+++ mediaLibrary-backend/api/controllers/notification.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const User =  require('../models/user');
+const Notification = require('../models/notification');
 
 exports.notifications_get_all = (req, res, next) =>{
     const id= req.userData.userId;
@@ -21,5 +22,45 @@ exports.notifications_get_all = (req, res, next) =>{
         });
     });
     
+}  
+exports.notifications_read = (req, res, next) =>{
+    Notification.findOne({_id: req.params.notificationId})
+    .then(result=>{
+        if (result===null){
+            res.status(404).json({
+                message: 'Notification not found'
+            })
+        }else{
+            Notification.updateOne({_id: req.params.notificationId},{state: 'read'})
+            .exec()
+            .then(result => {
+                res.status(200).json({
+                    message: 'State change successfully'
+                });
+            })
+        }
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+    });
 }
 
+exports.notifications_delete = (req, res, next) =>{
+    Notification.remove({_id: req.params.notificationId})
+    .exec()
+    .then(result=> {
+        console.log(result);
+        res.status(200).json({
+            message: 'Notification deleted'
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+}
